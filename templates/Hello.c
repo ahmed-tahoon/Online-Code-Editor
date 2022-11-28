cf81df1d008b0570d0cac1ae9bcd25b011010018
ahmed-tahoon <77838959+ahmed-tahoon@users.noreply.github.com>
2022-11-28 15:17:04 +0200

    Update Hello.c

--- templates/Hello.c
+++ templates/Hello.c
@@ -1,9 +1,9 @@
-// Online C++ compiler to run C++ program online
+// Online C compiler to run C program online
 #include <stdio.h>
 
 int main() {
-    // Write C++ code here
-    printf("Hello C++")
+    // Write C code here
+    printf("Hello C")
 
     return 0;
 }
